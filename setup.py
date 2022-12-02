d82f3a420b31e907d17a3ed5774b9572e76b1f31
Aki Van Ness <aki@lethalbit.net>
2022-12-02 18:32:29 -0500

    setup: updated setup scripts and fixed deps

--- setup.py
+++ setup.py
@@ -1,37 +1,61 @@
-# SPDX-License-Identifier: BSD-3-Clause
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-2-Clause
 
 from setuptools import setup, find_packages
+from pathlib    import Path
 
-def vcs_ver():
-	def scheme(version):
+REPO_ROOT   = Path(__file__).parent
+README_FILE = (REPO_ROOT / 'README.md')
+
+def scm_version():
+	def local_scheme(version):
 		if version.tag and not version.distance:
-			return version.format_with("")
+			return version.format_with('')
 		else:
-			return version.format_choice("+{node}", "+{node}.dirty")
+			return version.format_choice('+{node}', '+{node}.dirty')
 	return {
-		"relative_to": __file__,
-		"version_scheme": "guess-next-dev",
-		"local_scheme": scheme
+		'relative_to'   : __file__,
+		'version_scheme': 'guess-next-dev',
+		'local_scheme'  : local_scheme
 	}
 
+def doc_version():
+	try:
+		from setuptools_scm.git import parse as parse_git
+	except ImportError:
+		return ''
+
+	git = parse_git('.')
+	if not git:
+		return ''
+	elif git.exact:
+		return git.format_with('v{tag}')
+	else:
+		return 'latest'
+
 setup(
-	name            = 'sol',
-	use_scm_version = vcs_ver(),
-	author          = 'Katherine Temkin',
-	author_email    = 'k@ktemkin.com',
-	maintainer      = [
+	name             = 'sol',
+	use_scm_version  = scm_version(),
+	author           = 'Katherine Temkin',
+	author_email     = 'k@ktemkin.com',
+	maintainer       = ', '.join([
 		'Aki Van Ness',
 		'Rachel Mant',
-	],
-	maintainer_email= [
+	]),
+	maintainer_email = ', '.join([
 		'aki@lethalbit.net',
 		'git@dragonmux.network',
-	],
-	license         = 'BSD-3-Clause',
-	description     = 'Amaranth HDL framework for FPGA-based USB solutions',
-	python_requires = '~=3.9',
-	packages        = find_packages(),
-	setup_requires  = [
+	]),
+	license          = 'BSD-3-Clause',
+	description      = 'Torii HDL framework for FPGA-based USB solutions',
+	python_requires  = '~=3.9',
+	zip_safe         = True,
+	url              = 'https://github.com/shrine-maiden-heavy-industries/sol',
+
+	long_description = README_FILE.read_text(),
+	long_description_content_type = 'text/markdown',
+
+	setup_requires   = [
 		'wheel',
 		'setuptools',
 		'setuptools_scm',
@@ -40,28 +64,66 @@ setup(
 	install_requires = [
 		'pyserial~=3.5',
 		'pyvcd>=0.2.2,<0.4',
+		'rich~=12.6.0',
 
-		'usb-construct @ git+https://github.com/shrine-maiden-heavy-industries/usb-construct@main',
-		'amaranth @ git+https://github.com/amaranth-lang/amaranth.git@main',
-		'amaranth-boards @ git+https://github.com/amaranth-lang/amaranth-boards.git@main',
-		'amaranth-soc @ git+https://github.com/amaranth-lang/amaranth-soc.git@main',
-		'amaranth-stdio @ git+https://github.com/amaranth-lang/amaranth-stdio.git@main',
+		'usb-construct<1.0',
+		'torii<1.0',
 	],
 
 	extras_require = {
 		'dev': [
-			'tox~=3.22.0',
-		],
-		'soc': [
-			'lambdasoc @ git+https://github.com/shrine-maiden-heavy-industries/lambdasoc.git@main',
-			'minerva @ git+https://github.com/lambdaconcept/minerva.git',
+			'nox',
 		],
 		'platform': [
 			'pyusb~=1.2.0',
 			'libusb1~=1.9.2',
-			'apollo-fpga @ git+https://github.com/shrine-maiden-heavy-industries/apollo@main',
 			'prompt-toolkit~=3.0.16',
 			'ziglang~=0.8.0',
 		]
-	}
+	},
+
+	packages         = find_packages(
+		where   = '.',
+		exclude = (
+			'tests',
+			'tests.*'
+		)
+	),
+	package_data      = {
+		'sol': [
+			'py.typed'
+		],
+	},
+	classifiers       = [
+		'Development Status :: 4 - Beta',
+
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: Science/Research',
+
+		'License :: OSI Approved :: BSD License',
+
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX :: Linux',
+
+		'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
+		'Programming Language :: Python :: 3.11',
+
+		'Topic :: Scientific/Engineering',
+		'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+		'Topic :: Software Development',
+		'Topic :: Software Development :: Embedded Systems',
+		'Topic :: Software Development :: Libraries',
+		'Topic :: System :: Hardware :: Universal Serial Bus (USB)',
+
+		'Typing :: Typed',
+	],
+
+	project_urls     = {
+		'Documentation': 'https://shrine-maiden-heavy-industries.github.io/sol/',
+		'Source Code'  : 'https://github.com/shrine-maiden-heavy-industries/sol',
+		'Bug Tracker'  : 'https://github.com/shrine-maiden-heavy-industries/sol/issues',
+	},
 )
