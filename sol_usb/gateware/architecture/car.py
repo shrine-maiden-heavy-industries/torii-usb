70d9faef7e46eb053a7bc0fb9a0abee8c9c6cf86
Aki Van Ness <aki@lethalbit.net>
2023-10-21 08:58:40 -0700

    gateware: extracted the remaining tests from their modules into the `tests` module tree

--- sol_usb/gateware/architecture/car.py
+++ sol_usb/gateware/architecture/car.py
@@ -13,7 +13,6 @@ from torii       import (
 	ClockDomain, ClockSignal, Elaboratable, Instance, Module, ResetSignal, Signal
 )
 
-from ..test      import SolGatewareTestCase, sync_test_case
 from ..utils.cdc import stretch_strobe_signal
 
 
@@ -102,41 +101,6 @@ class PHYResetController(Elaboratable):
 
 
 		return m
-
-
-
-class PHYResetControllerTest(SolGatewareTestCase):
-	FRAGMENT_UNDER_TEST = PHYResetController
-
-	def initialize_signals(self):
-		yield self.dut.trigger.eq(0)
-
-	@sync_test_case
-	def test_power_on_reset(self):
-
-		#
-		# After power-on, the PHY should remain in reset for a while.
-		#
-		yield
-		self.assertEqual((yield self.dut.phy_reset), 1)
-
-		yield from self.advance_cycles(30)
-		self.assertEqual((yield self.dut.phy_reset), 1)
-
-		yield from self.advance_cycles(60)
-		self.assertEqual((yield self.dut.phy_reset), 1)
-
-		#
-		# Then, after the relevant reset time, it should resume being unasserted.
-		#
-		yield from self.advance_cycles(31)
-		self.assertEqual((yield self.dut.phy_reset), 0)
-		self.assertEqual((yield self.dut.phy_stop),  1)
-
-		yield from self.advance_cycles(120)
-		self.assertEqual((yield self.dut.phy_stop),  0)
-
-
 
 class SolDomainGenerator(Elaboratable, metaclass = ABCMeta):
 	'''
---
+++ tests/utils/test_cdc.py
@@ -0,0 +1,83 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+from unittest      import TestCase
+
+from torii         import Module, Record, Signal
+from torii.hdl.rec import DIR_FANIN, DIR_FANOUT
+
+from sol_usb.gateware.test      import SolGatewareTestCase, sync_test_case
+from sol_usb.gateware.utils.cdc import stretch_strobe_signal, synchronize
+
+class StrobeStretcherTest(SolGatewareTestCase):
+	''' Test case for our strobe stretcher function. '''
+
+
+	def instantiate_dut(self):
+		m = Module()
+
+		# Create a module that only has our stretched strobe signal.
+		m.strobe_in = Signal()
+		m.stretched_strobe = stretch_strobe_signal(m, m.strobe_in, to_cycles = 2)
+
+		return m
+
+
+	def initialize_signals(self):
+		yield self.dut.strobe_in.eq(0)
+
+
+	@sync_test_case
+	def test_stretch(self):
+
+		# Ensure our stretched strobe stays 0 until it sees an input.
+		yield
+		self.assertEqual((yield self.dut.stretched_strobe), 0)
+		yield
+		self.assertEqual((yield self.dut.stretched_strobe), 0)
+
+		# Apply our strobe, and validate that we immediately see a '1'...
+		yield self.dut.strobe_in.eq(1)
+		yield
+		self.assertEqual((yield self.dut.stretched_strobe), 1)
+
+		# ... ensure that 1 lasts for a second cycle ...
+		yield self.dut.strobe_in.eq(0)
+		yield
+		self.assertEqual((yield self.dut.stretched_strobe), 1)
+
+		# ... and then returns to 0.
+		yield
+		self.assertEqual((yield self.dut.stretched_strobe), 0)
+
+		yield
+		self.assertEqual((yield self.dut.stretched_strobe), 0)
+
+
+
+class SynchronizedTest(TestCase):
+
+	def test_signal(self):
+		m = Module()
+		synchronize(m, Signal())
+
+	def test_directional_record(self):
+		m = Module()
+
+		record = Record([
+			('sig_in',  1, DIR_FANIN),
+			('sig_out', 1, DIR_FANOUT)
+		])
+		synchronize(m, record)
+
+	def test_nested_record(self):
+		m = Module()
+
+		record = Record([
+			('sig_in',  1, DIR_FANIN),
+			('sig_out', 1, DIR_FANOUT),
+			('nested', [
+				('subsig_in',  1, DIR_FANIN),
+				('subsig_out', 1, DIR_FANOUT),
+			])
+		])
+		synchronize(m, record)
